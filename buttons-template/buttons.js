0bc2f715213aa1de2fa5c42ee9eda5397e9f0bd3
Jason R <99057964@mydavinci.nl>
2020-11-24 12:33:35 +0100

    Ik ga de functies anders maken.

--- buttons-template/buttons.js
+++ buttons-template/buttons.js
@@ -11,24 +11,6 @@ var button3 = document.getElementById("button3");
 var bigImage = document.getElementById("image1");
 var tinyImage = document.getElementById("image2");
 
-var text = 0;
-
-function waarde0(){
-    bigImage.src = "images/bg1.jpg";//De image veranderd zodra er op de button word gedrukt.
-    tinyImage.src = "images/1.jpg";//De image veranderd zodra er op de button word gedrukt.
-    text = 0;
-    document.getElementById("button1").value = text++;
-    
-}
-
-function waarde1(){
-    text.textContent = 1;//Locale variabele voor deze functie,
-    bigImage.src = "images/bg2.jpg";//De image veranderd zodra er op de button word gedrukt.
-    tinyImage.src = "images/2.jpg";//De image veranderd zodra er op de button word gedrukt.
-}
-
-function waarde2(){
-    text.textContent = 1;//Locale variabele voor deze functie,
-    bigImage.src = "images/bg3.jpg";//De image veranderd zodra er op de button word gedrukt.
-    tinyImage.src = "images/3.jpg";//De image veranderd zodra er op de button word gedrukt.
-}
+button1.addEventListener('click',function(){
+    alert('test');
+});
