4098d4e090e1c5c4a3ddacc11002713b67157321
Jason R <99057964@mydavinci.nl>
2020-11-24 12:48:28 +0100

    Heb counters toegevoegd en de lab opdracht af.
    Heb counters toegevoegd zodat de teller boven de plaatsjes met de waardes steeds 1 omhoog gaan.

--- buttons-template/buttons.js
+++ buttons-template/buttons.js
@@ -11,6 +11,25 @@ var button3 = document.getElementById("button3");
 var bigImage = document.getElementById("image1");
 var tinyImage = document.getElementById("image2");
 
+//Variabelen voor de counters voor hoeveel keer op de knop word gedrukt.
+var count1 = 0;
+var count2 = 0;
+var count3 = 0;
+
 button1.addEventListener('click',function(){
-    alert('test');
+    button1.innerHTML = count1++;//Elke keer als er op 1 van de buttons worden gedrukt gaat er steeds 1 bij komen.
+    bigImage.src = "images/bg1.jpg";
+    tinyImage = "images/1.jpg";
+});
+
+button2.addEventListener('click',function(){
+    button2.innerHTML = count2++;//Zie regel 20!.
+    bigImage.src = "images/bg2.jpg";
+    tinyImage = "images/2.jpg";
+});
+
+button3.addEventListener('click',function(){
+    button3.innerHTML = count3++;//Zie regel 20!.
+    bigImage.src = "images/bg3.jpg";
+    tinyImage = "images/3.jpg";
 });
