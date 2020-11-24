d8fb2fa9017adc6a2b84b7671bd571321963e26c
Jason R <99057964@mydavinci.nl>
2020-11-24 12:49:44 +0100

    Vergeten om bij de kleine plaatjes de src bij te zetten.

--- buttons-template/buttons.js
+++ buttons-template/buttons.js
@@ -12,24 +12,24 @@ var bigImage = document.getElementById("image1");
 var tinyImage = document.getElementById("image2");
 
 //Variabelen voor de counters voor hoeveel keer op de knop word gedrukt.
-var count1 = 0;
-var count2 = 0;
-var count3 = 0;
+var count1 = 1;
+var count2 = 1;
+var count3 = 1;
 
 button1.addEventListener('click',function(){
     button1.innerHTML = count1++;//Elke keer als er op 1 van de buttons worden gedrukt gaat er steeds 1 bij komen.
     bigImage.src = "images/bg1.jpg";
-    tinyImage = "images/1.jpg";
+    tinyImage.src = "images/1.jpg";
 });
 
 button2.addEventListener('click',function(){
     button2.innerHTML = count2++;//Zie regel 20!.
     bigImage.src = "images/bg2.jpg";
-    tinyImage = "images/2.jpg";
+    tinyImage.src = "images/2.jpg";
 });
 
 button3.addEventListener('click',function(){
     button3.innerHTML = count3++;//Zie regel 20!.
     bigImage.src = "images/bg3.jpg";
-    tinyImage = "images/3.jpg";
+    tinyImage.src = "images/3.jpg";
 });
